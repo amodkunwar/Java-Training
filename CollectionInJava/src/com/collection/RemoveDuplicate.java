bb4d0f87f8c79d000d472e0dd305a06e0aff24bd
Amod Kunwar <43563708+amodkunwar@users.noreply.github.com>
2021-05-24 11:52:21 +0530

    Remove duplicates from list

--- CollectionInJava/src/com/collection/RemoveDuplicate.java
+++ CollectionInJava/src/com/collection/RemoveDuplicate.java
@@ -17,11 +17,31 @@ public class RemoveDuplicate {
 		lst.add(5);
 		lst.add(5);
 
-		List<Integer> collect = lst.stream().distinct().collect(Collectors.toList());
+//		List<Integer> collect = lst.stream().distinct().collect(Collectors.toList());
+//
+//		for (Integer integer : collect) {
+//			System.out.println(integer);
+//		}
 
-		for (Integer integer : collect) {
+		System.out.println("Before removing duplicates");
+		for (Integer integer : lst) {
 			System.out.println(integer);
 		}
+
+		System.out.println("After removing duplicates");
+		for (int i = 0; i < lst.size(); i++) {
+			for (int j = i + 1; j < lst.size(); j++) {
+				if (lst.get(i).equals(lst.get(j))) {
+					lst.remove(j);
+					j--;
+				}
+			}
+		}
+
+		for (Integer integer : lst) {
+			System.out.println(integer);
+		}
+
 	}
 
 }
