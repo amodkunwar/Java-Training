04fdfab93b7db44a8bbee05cb0abeb35aa7d760d
Amod Kunwar <43563708+amodkunwar@users.noreply.github.com>
2021-05-27 11:16:58 +0530

    Spring boot

--- Demo/src/main/java/com/example/demo/immutable/ImmutableExample1.java
+++ Demo/src/main/java/com/example/demo/immutable/ImmutableExample1.java
@@ -8,16 +8,13 @@ public final class ImmutableExample1 {
 		this.panCardNumber = panCardNumber;
 	}
 
-	public String getPanCard() {
+	public String getPan() {
 		return this.panCardNumber;
 	}
-
+	
 	public static void main(String[] args) {
 		ImmutableExample1 example1 = new ImmutableExample1("12345");
-		System.out.println("Example 1 : " + example1);
-
-		ImmutableExample1 example2 = new ImmutableExample1("54321");
-		System.out.println("Example 2 : " + example2);
+		System.out.println(example1.getPan());
 	}
 
 }
